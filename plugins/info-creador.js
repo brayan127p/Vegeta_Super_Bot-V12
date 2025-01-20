298b84d1e24ad87eaec808e5d4cc58a11170583f
Legend <164983870+Botname4@users.noreply.github.com>
2025-01-19 22:44:49 -0400

    Add files via upload

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,24 +1,25 @@
 import PhoneNumber from 'awesome-phonenumber'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-m.react('☁️')
+//m.react('⚙️')
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/3kbbok.jpg')
-let biografia = await conn.fetchStatus('584120346669' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/tm6axp.jpg')
+let biografia = await conn.fetchStatus('5491166401905' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 let name = await conn.getName(who)
 
   await sendContactArray(conn, m.chat, [
-     [`${nomorown}`, `👑 Propietario`, `☁️ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜`, dev, 'thekingdestroy507@gmail.com', `🇻🇪 Venezuela`, `https://github.com/The-King-Destroy`, bio],
-[`${conn.user.jid.split('@')[0]}`, `Es Un Bot 🍬`, `${packname}`, `📵 No Hacer Spam`, 'moisesmusic04@gmail.com', `🇨🇴 Colombia`, `https://github.com/The-King-Destroy/Yuki_Suou-Bot`, biobot]
+     [`${nomorown}`, `👑 Propietario`, `💨 ꧁༒ĹєǤ𝒆𝐧𝐃༒꧂`, dev, 'darel0954@gmail.com', `🇻🇪 Venezuela`, `${global.yt}`, bio],
+[`${conn.user.jid.split('@')[0]}`, `Es Un Bot 🐉`, `${packname}`, `📵 No Hacer Spam`, 'https://youtube.com/@kakaroto-bot?si=dRYFi-dAw8H_mgGI', `Planeta Vegeta 🌍`, `https://github.com/Diomar-s/Kakaroto-Bot-MD`, biobot]
 ], m)
+  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
   } 
 
 handler.help = ["creador","owner"]
 handler.tags = ["info"]
-handler.command = ['creador', 'owner']
+handler.command = /^(owner|creador)$/i
 export default handler
 
 async function sendContactArray(conn, jid, data, quoted, options) {
@@ -28,6 +29,7 @@ async function sendContactArray(conn, jid, data, quoted, options) {
             number = number.replace(/[^0-9]/g, '')
             let njid = number + '@s.whatsapp.net'
             let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
             let vcard = `
 BEGIN:VCARD
 VERSION:3.0
@@ -57,4 +59,4 @@ END:VCARD`.trim()
             quoted,
             ...options
         })
-}
+        }
