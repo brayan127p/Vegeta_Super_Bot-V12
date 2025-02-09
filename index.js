249d9b50180d40ab7fbb363f16379a370cfab42b
Legend <164983870+Botname4@users.noreply.github.com>
2025-02-09 15:27:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Yuki-Bot-MD', {
+say('Vegeta-Super_Bot', {
 font: 'block',
 align: 'center',
 colors: ['yellow']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['redBright']
 });
-say(`Developed By • The-King-Destroy`, {
+say(`Developed By • Brayan`, {
 font: 'console',
 align: 'center',
 colors: ['blueBright']
