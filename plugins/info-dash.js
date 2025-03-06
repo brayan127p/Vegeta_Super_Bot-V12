32f367fbec5354d133b02232cc07da1094035f43
Elrebelde1 <sebastianbarbaro82@gmail.com>
2025-03-06 17:10:29 -0400

    Actualizar info-dash.js

--- plugins/info-dash.js
+++ plugins/info-dash.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   const dashLink = 'https://dash.luminarys.shop'; //
 
   // Envía el enlace como respuesta
-  conn.reply(m.chat, `Aquí tienes el enlace a tu dashboard: ${dashLink}`, m);
+  conn.reply(m.chat, `Aquí tienes el enlace del Dash del host luminar'ys: ${dashLink}`, m);
 };
 
 handler.command = ['dash', 'enviardash'];
