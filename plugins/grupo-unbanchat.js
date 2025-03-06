fcaeba12effd860685c3f1a53ad3aabad69eb118
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-05 22:47:00 -0800

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -8,8 +8,8 @@ await conn.reply(m.chat, '🍬 *¡Vegeta-Bot ya fué desbaneada en este chat!*',
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
 handler.command = ['unbanchat','desbanearchat','desbanchat']
-handler.User = true 
-handler.botUser = true
+handler.Admin = true 
+handler.botAdmin = true
 handler.group = true
 
 export default handler
