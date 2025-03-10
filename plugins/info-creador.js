3273cee80f3a953c1b176c97fff2d2199d8c8f82
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-09 22:13:14 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,37 +1,27 @@
-//código creado por Deylin 
+// código creado por Deylin 
 // https://github.com/deylinqff
 // no quites créditos 
 
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) { 
-    let numcreador = '526641804242';
+    let numcreador = '526641804242'; 
     let ownerJid = numcreador + '@s.whatsapp.net';
 
+   
     let name = await conn.getName(ownerJid) || 'Deylin'; 
     let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
 
-    let empresa = 'ᑲrᥲᥡᥲᥒ- Servicios Tecnológicos';
 
     let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:;${name};;;
 FN:${name}
-ORG:${empresa};
-TITLE:CEO & Fundador
 TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
-EMAIL:correo@empresa.com
-URL:https://www.tuempresa.com
 NOTE:${about}
-ADR:;;Dirección de tu empresa;;;;
-X-ABADR:ES
-X-ABLabel:Dirección Web
-X-ABLabel:Correo Electrónico
-X-ABLabel:Teléfono de contacto
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
 END:VCARD`.trim();
+
 
     await conn.sendMessage(m.chat, { 
         contacts: { 
