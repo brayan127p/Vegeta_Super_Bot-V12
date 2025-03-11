02139444d0171f71fea69f1a82cf1b255569ba46
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-10 21:06:29 -0700

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,7 +5,7 @@
 import PhoneNumber from 'awesome-phonenumber';
 
 async function handler(m, { conn }) { 
-    let numcreador = '526641804242'; 
+    let numcreador = '524623467530'; 
     let ownerJid = numcreador + '@s.whatsapp.net';
 
    
