258e2f39c1cc710519f269eb0488454c049bdc34
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-13 20:00:05 -0700

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,184 +1,77 @@
-import axios from "axios";
-import cheerio from "cheerio";
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) {
-    return conn.reply(m.chat, `Usa el formato: ${usedPrefix + command} <enlace de TikTok>`, m);
-  }
-
+import fg from 'api-dylux';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import {tiktok} from '@xct007/frieren-scraper';
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+import {tiktokdl} from '@bochilteam/scraper';
+//const CFROSAPI = global.APIs.CFROSAPI;
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+//m.react('🍁') 
+  if (!text) throw `_*< DESCARGAS - TIKTOK />*_\n\n*☁️ Iɴɢʀᴇsᴇ Uɴ Eɴʟᴀᴄᴇ Dᴇ Vɪᴅᴇᴏ Dᴇ TɪᴋTᴏᴋ.*\n\n*☁ Eᴊᴇᴍᴘʟᴏ:* _${usedPrefix + command} https://www.tiktok.com/@xaiyekz/video/7368941682944953606_`;
+  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `*< DESCARGAS - TIKTOK />*\n\n*☁️ Iɴɢʀᴇsᴇ Uɴ Eɴʟᴀᴄᴇ Dᴇ Vɪᴅᴇᴏ Dᴇ Tɪᴋᴛᴏᴋ.*\n\n*☁ Eᴊᴇᴍᴘʟᴏ:* _${usedPrefix + command} https://www.tiktok.com/@xaiyekz/video/7368941682944953606_`;
+  const texto = `_💛 @${m.sender.split`@`[0]}  ᩭ✎Enviando Video, espere un momento...._`;
+  // let buttons = [{ buttonText: { displayText: '♫ 𝙰𝚄𝙳𝙸𝙾 ♫' }, buttonId: `${usedPrefix}tomp3` }]
   try {
-    await m.react('🕒');
-
-    const videoResult = await ttsave.video(text);
-    const { 
-      type, 
-      nickname, 
-      username, 
-      description, 
-      videoInfo, 
-      slides, 
-      audioUrl 
-    } = videoResult;
-
-    let message = `*✔️🍟Downloader tiktok.*
-
-> • *Nombre*: ${nickname || "-"}
-> • *Usuario*: ${username || "-"}
-> • *Descripción*: ${description || "-"}
-`.trim();
-
-    if (type === "slide") {
-      message += "\n> • *Tipo*: Presentación (Imágenes)";
-      await conn.reply(m.chat, message, m);
-
-      for (let slide of slides) {
-        await m.react('✅');
-        await conn.sendFile(m.chat, slide.url, `presentación-${slide.number}.jpg`, "", m);
-      }
-    } 
-    else if (type === "video") {
-      message += "\n> • *Tipo*: Video";
-
-      if (videoInfo.nowm) {
-        await m.react('✅');
-await conn.sendMessage(m.chat, {
-  video: { url: videoInfo.nowm },
-  caption: message,
-  footer: dev,
-  buttons: [
-    {
-      buttonId: `.tiktokmp3 ${text}`,
-      buttonText: {
-        displayText: 'Audio 🎧',
-      },
-    },
-    {
-      buttonId: `.tiktokhd ${text}`,
-      buttonText: {
-        displayText: 'Calidad HD',
-      },
-    },
-  ],
-  viewOnce: true,
-  headerType: 4,
-}, { quoted: m });
-      } else {
-        conn.reply(m.chat, "No se pudo obtener el video sin marca de agua.", m);
+m.react('🍁') 
+    const aa = {quoted: m, userJid: conn.user.jid};
+    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: packname, body: wm, thumbnail: icons, sourceUrl: yt}, mentionedJid: [m.sender]}}}, aa);
+    await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
+    const dataFn = await conn.getFile(`${CFROSAPI}/api/tiktokv2?url=${args[0]}`);
+    const desc1n = `_💛  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+    await conn.sendMessage(m.chat, {video: dataFn.data, caption: desc1n}, {quoted: fkontak});
+  } catch (ee1) {
+  try {
+    //const aa = {quoted: m, userJid: conn.user.jid};
+    //const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: packname, body: wm, thumbnail: icons, sourceUrl: yt}, mentionedJid: [m.sender]}}}, aa);
+    //await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
+    const dataF = await tiktok.v1(args[0]);
+    // let desc1 =  `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${dataF.nickname || 'Indefinido'}`
+    const desc1 = `_💛  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+    await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: fkontak});
+  } catch (e1) {
+    try {
+      const tTiktok = await tiktokdlF(args[0]);
+      // let desc2 = `🔗 *Url:* ${tTiktok.video}`
+      const desc2 = `_💛  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+      await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: fkontak});
+    } catch (e2) {
+      try {
+        const p = await fg.tiktok(args[0]);
+        // let te = `*𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴:* ${p.author || 'Indefinido'}`
+        const te = `_💛  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+        await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: te}, {quoted: fkontak});
+      } catch (e3) {
+        try {
+          const {author: {nickname}, video, description} = await tiktokdl(args[0]);
+          const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+          // let cap = `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${nickname || 'Indefinido'}`
+          const cap = `_💛  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
+          await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: fkontak});
+        } catch {
+          throw `_*< DESCARGAS - TIKTOK />*_\n\n*🚫 Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
+          }
+        }
       }
     }
-
-    if (audioUrl) {
-    }
-  } catch (error) {
-    console.error(error);
-    conn.reply(m.chat, `Ocurrió un error al procesar la solicitud. Asegúrate de que el enlace de TikTok sea válido e inténtalo nuevamente.`, m);
   }
 };
 
-handler.help = ["tiktok *<url>*"];
-handler.tags = ["dl"];
-handler.command = ["tiktok"];
+handler.command = ['tiktok2', 'ttk2', 'tt2'];
+handler.estrellas = 4;
+handler.register = true;
 export default handler;
 
-const headers = {
-  authority: "ttsave.app",
-  accept: "application/json, text/plain, */*",
-  origin: "https://ttsave.app",
-  referer: "https://ttsave.app/en",
-  "user-agent": "Postify/1.0.0",
-};
-
-const ttsave = {
-  submit: async function (url, referer) {
-    const headerx = { ...headers, referer };
-    const data = { query: url, language_id: "1" };
-    return axios.post("https://ttsave.app/download", data, { headers: headerx });
-  },
-
-  parse: function ($) {
-    const uniqueId = $("#unique-id").val();
-    const nickname = $("h2.font-extrabold").text();
-    const profilePic = $("img.rounded-full").attr("src");
-    const username = $("a.font-extrabold.text-blue-400").text();
-    const description = $("p.text-gray-600").text();
-
-    const dlink = {
-      nowm: $("a.w-full.text-white.font-bold").first().attr("href"),
-      wm: $("a.w-full.text-white.font-bold").eq(1).attr("href"),
-      audio: $("a[type='audio']").attr("href"),
-      profilePic: $("a[type='profile']").attr("href"),
-      cover: $("a[type='cover']").attr("href"),
-    };
-
-    const stats = {
-      reproducciones: "",
-      meGusta: "",
-      comentarios: "",
-      compartidos: "",
-    };
-
-    $(".flex.flex-row.items-center.justify-center").each((index, element) => {
-      const $element = $(element);
-      const svgPath = $element.find("svg path").attr("d");
-      const value = $element.find("span.text-gray-500").text().trim();
-
-      if (svgPath && svgPath.startsWith("M10 18a8 8 0 100-16")) {
-        stats.reproducciones = value;
-      } else if (svgPath && svgPath.startsWith("M3.172 5.172a4 4 0 015.656")) {
-        stats.meGusta = value || "0";
-      } else if (svgPath && svgPath.startsWith("M18 10c0 3.866-3.582")) {
-        stats.comentarios = value;
-      } else if (svgPath && svgPath.startsWith("M17.593 3.322c1.1.128")) {
-        stats.compartidos = value;
-      }
-    });
-
-    const tituloCancion = $(".flex.flex-row.items-center.justify-center.gap-1.mt-5")
-      .find("span.text-gray-500")
-      .text()
-      .trim();
-
-    const slides = $("a[type='slide']")
-      .map((i, el) => ({
-        number: i + 1,
-        url: $(el).attr("href"),
-      }))
-      .get();
-
-    return {
-      uniqueId,
-      nickname,
-      profilePic,
-      username,
-      description,
-      dlink,
-      stats,
-      tituloCancion,
-      slides,
-    };
-  },
-
-  video: async function (link) {
-    try {
-      const response = await this.submit(link, "https://ttsave.app/en");
-      const $ = cheerio.load(response.data);
-      const result = this.parse($);
-
-      if (result.slides && result.slides.length > 0) {
-        return { type: "slide", ...result };
-      }
-
-      return {
-        type: "video",
-        ...result,
-        videoInfo: {
-          nowm: result.dlink.nowm,
-          wm: result.dlink.wm,
-        },
-      };
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  },
-};
+async function tiktokdlF(url) {
+  if (!/tiktok/.test(url)) return `_*< 𝐷𝐸𝑆𝐶𝐴𝑅𝐺𝐴𝑆 - 𝑇𝐼𝐾𝑇𝑂𝐾 />*_\n\n*🍁 Iɴɢʀᴇsᴇ Uɴ Eɴʟᴀᴄᴇ Dᴇ Tɪᴋᴛᴏᴋ.*\n\n*☁️ Eᴊᴇᴍᴘʟᴏ:* _${usedPrefix + command} https://www.tiktok.com/@xaiyekz/video/7368941682944953606_`;
+  const gettoken = await axios.get('https://tikdown.org/id');
+  const $ = cheerio.load(gettoken.data);
+  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr( 'value' );
+  const param = {url: url, _token: token};
+  const {data} = await axios.request('https://tikdown.org/getAjax?', {method: 'post', data: new URLSearchParams(Object.entries(param)), headers: {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}});
+  const getdata = cheerio.load(data.html);
+  if (data.status) {
+    return {status: true, thumbnail: getdata('img').attr('src'), video: getdata('div.download-links > div:nth-child(1) > a').attr('href'), audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')};
+  } else {
+    return {status: false};
+  }
+}
