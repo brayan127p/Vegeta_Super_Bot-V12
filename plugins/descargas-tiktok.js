754e494d6b97f756efb7aa2dfc38ba460415950d
Brayan Martínez <lourdesagueda93@gmail.com>
2025-03-18 22:52:09 -0700

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -18,7 +18,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
 
         if (videoURL) {
             await conn.sendFile(m.chat, videoURL, "tiktok.mp4", ` ─━─━─≪✠≫─━─━─━╮
-   &{emoji} *VEGETA-BOT*
+   ${emoji} *VEGETA-BOT*
 ╰━─━─━─≪✠≫─━─━─━╯\n⟢ 𝑨𝒌𝒊 𝒕𝒊𝒆𝒏𝒆𝒔: *˙★˙†*\n⟢ 𝑫𝒊𝒔𝒇𝒓𝒖𝒕𝒂.`, m);
         } else {
             return conn.reply(m.chat, "No se pudo descargar.", m);
